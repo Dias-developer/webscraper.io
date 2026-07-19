025d35c2c99c1d66f30b14232c0714eadddde658
Dias <kasenov_06@internet.ru>
2026-07-19 20:30:52 +0500

    added checking web scraping time

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # importing
 from parsing import parsing
 from save import save_excel
-
+import time
 # url
 url = 'https://webscraper.io/test-sites/pagination'
 
 # web scraping
+start = time.time()
 parsed = parsing(url)
-
+end = time.time()
 # save in Excel
 save_excel(parsed)
+print(f"Время выполнения: {end - start:.2f} секунд")
