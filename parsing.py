705db800df6879c4f52726be696dcad99bb1c4b1
Dias <kasenov_06@internet.ru>
2026-07-20 23:04:44 +0500

    fixed bugs and increased a speed

--- parsing.py
+++ parsing.py
@@ -1,12 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor # Для многопоточности
+import threading
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.0.0'}
 
-def parse_product(link):
+def make_session(): # Для создания сессий для каталога
+    session = requests.Session()
+    session.headers.update(headers)
+    return session
+
+
+
+thread_local = threading.local()
+def get_session(): # Для создания сессий для товаров
+    if not hasattr(thread_local, 'session'):
+        thread_local.session = make_session()
+    return thread_local.session
+
+
+
+def parse_product(link): # Парсит товары
     try:
-        response = requests.get(link, headers=headers)
+        session = get_session()
+        response = session.get(link, timeout=10)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'lxml')
@@ -27,34 +44,31 @@ def parse_product(link):
         print(f"Ошибка {link}: {e}")
         return None
 
-def parsing(url):
+
+
+def parsing(url): # Парсит каталог с пагинацией
     products = []
-    session = requests.Session()
-    session.headers.update(headers)
-    while True:
-        response = session.get(url, timeout=10)
-        response.raise_for_status()
+    session = make_session()
+    with ThreadPoolExecutor(max_workers=10) as executor:
 
-        catalog_soup = BeautifulSoup(response.text, "lxml")
+        while True:
+            response = session.get(url, timeout=10)
+            response.raise_for_status()
 
-        collect_links = catalog_soup.find_all('a', class_='card-head-url')
-        links = []
-        for link in collect_links:
-           href = link.get('href')
-           if href:
-               links.append(href)
+            catalog_soup = BeautifulSoup(response.text, "lxml")
 
-        with ThreadPoolExecutor(max_workers=10) as executor:
+            collect_links = catalog_soup.find_all('a', class_='card-head-url')
+            links = [link.get('href') for link in collect_links if link.get('href')]
+
             results = executor.map(parse_product, links)
 
             for product in results:
                 if product:
                     products.append(product)
 
-        next_b = catalog_soup.find('a', class_='page-link next')
-        if next_b:
-            url = next_b.get('href')
-        else:
-            break
-
+            next_b = catalog_soup.find('a', class_='page-link next')
+            if next_b:
+                url = next_b.get('href')
+            else:
+                break
     return products
